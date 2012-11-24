076355183aa08fe7b73ece94b1bd3ae9a4a1df0b
Dan LaManna <dan.lamanna@gmail.com>
2012-11-23 21:11:10 -0500

    Adding docs.

--- wordpress-mode.el
+++ wordpress-mode.el
@@ -1,3 +1,28 @@
+;;; wordpress-mode.el --- A minor mode for speeding up WordPress development in emacs.
+;; Copyright (C) 2012 Dan LaManna
+
+;; Author: Dan LaManna <dan.lamanna@gmail.com>
+;; Keywords: wordpress mode
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; Untested in multiple environments. Radically relies on calling php via the command
+;; line. Requires jump library for quicker navigation.
+
+;;; Code:
+
 (require 'jump)
 (require 'ido)
 (require 'json)
@@ -97,3 +122,5 @@
 	    (copy-directory orig-theme-dir new-theme-dir nil nil t)))))))
 
 (provide 'wordpress-mode)
+
+;;; wordpress-mode.el ends here
