6425212efe67757f378dd1cf54938362f7864a51
Dan LaManna <dan.lamanna@gmail.com>
2012-11-27 20:50:19 -0500

    Fixing issue with wp/exists throwing errors.

--- wordpress-mode.el
+++ wordpress-mode.el
@@ -60,7 +60,8 @@
   "Given the current buffer contains a file, this returns
    the absolute path for the WordPress installation, or `nil'."
   (when (buffer-file-name)
-    (expand-file-name (locate-dominating-file (file-name-directory (buffer-file-name)) wp/config-file))))
+    (if (locate-dominating-file (file-name-directory (buffer-file-name)) wp/config-file)
+	(expand-file-name (locate-dominating-file (file-name-directory (buffer-file-name)) wp/config-file)))))
 
 (defun wp/shell-command(command)
   "Runs COMMAND using `wp/php-executable' -r after requiring wp-blog-header.php, COMMAND
